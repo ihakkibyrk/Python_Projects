9a61a11e9658a8547a1fbf04ec1441c2273749e6
ibrahim hakki bayrak <75913024+ihakkibyrk@users.noreply.github.com>
2021-02-14 03:38:34 +0100

    Add files via upload
    "FizzBuzz is a famous code challenge used in interviews to test basic programming skills. It's time to write your own implementation.
    Print numbers from 1 to 100 inclusively following these instructions:
    if a number is multiple of 3, print "Fizz" instead of this number,
    if a number is multiple of 5, print "Buzz" instead of this number,
    for numbers that are multiples of both 3 and 5, print "FizzBuzz",
    print the rest of the numbers unchanged."

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+
+def fizz_buzz():
+    number = int(input("Write a number " ))
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print("uncharged")
+
+fizz_buzz()
