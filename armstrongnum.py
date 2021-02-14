69bebe7e1cf616fe019002d931a7495a9d508958
IbrahimBayrak <i.hakkibyrk@gmail.com>
2021-02-14 22:33:47 +0100

    Armstrong number added

--- armstrongnum.py
+++ armstrongnum.py
@@ -2,15 +2,18 @@ number = str(input("Please write a numeric number : "))
 
 sum_num = 0
 
-if int(number) < 0 :
-    print(number,'it is an invalid entry. Don\'t use non-numeric, float, or negative values!')
+if number.isdecimal():
+    
+    if type(float(number)) == type(1.2):
+        for x in range(len(number)):
+            sum_num += int(number[x])**len(number)
+        if sum_num == int(number):
+            print(number, 'is an Armstrong number')
+        else:
+            print(number, 'is not an Armstrong number')
+   
 else:
-    for x in range(len(number)):
-        sum_num += int(number[x])**len(number)
-    if sum_num == int(number):
-        print(number, 'is an Armstrong number')
-    else:
-        print(number, 'is not an Armstrong number')
+    print(number,'it is an invalid entry. Don\'t use non-numeric, float, or negative values!')
 
 
 
